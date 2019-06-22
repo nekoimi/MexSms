5d8cb05425eb7cd3ad7066a49534d83a6376586a
yprisoner <yyprisoner@gmail.com>
2019-06-22 21:45:42 +0800

    更新配置文件路径获取方式

--- src/Providers/MexsmsServiceProvider.php
+++ src/Providers/MexsmsServiceProvider.php
@@ -27,7 +27,20 @@ class MexsmsServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes(array(
-            __DIR__.'/../../config/mexsms.php' => config_path('mexsms.php'),
+            __DIR__.'/../../config/mexsms.php' => $this->getConfigPath('mexsms.php'),
         ));
     }
+
+    /**
+     *
+     * @param string|null $file_name
+     * @return string
+     */
+    protected function getConfigPath(string $file_name = null) {
+        if (is_null($file_name)) {
+            return $this->app->basePath() . '/config';
+        }
+        return $this->app->basePath() . '/config/' . ltrim($file_name, '/');
+    }
+
 }
