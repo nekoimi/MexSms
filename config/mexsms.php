658b19e6efa4d2c041fa8ab84f972c9b66dcdaa5
yprisoner <yyprisoner@gmail.com>
2019-06-22 13:16:11 +0800

    update

--- config/mexsms.php
+++ config/mexsms.php
@@ -28,20 +28,17 @@ return array(
 
     // LeanCloud配置
     'leancloud' => [
-        'sms_request_url'   =>  env('MEXSMS_REQUEST_URL', 'https://bfujvuqa.api.lncld.net/1.1/requestSmsCode'),
-        'sms_verify_url'    =>  env('MEXSMS_VERIFY_URL', 'https://bfujvuqa.api.lncld.net/1.1/verifySmsCode'),
-
-        'sms_app_id'        =>  env('MEXSMS_LEANCLOUD_APP_ID', 'bfujvuqa'),
-        'sms_lc_id'         =>  env('MEXSMS_LEANCLOUD_LC_ID', 'BFUjvUQaqBKSd7ssrjvlbgxy-gzGzoHsz'),
-        'sms_lc_key'        =>  env('MEXSMS_LEANCLOUD_LC_KEY', 'LBquKn8IIKCe23nH3GGi81SB')
+        'sms_app_id'        =>  env('MEXSMS_LEANCLOUD_APP_ID', ''),
+        'sms_lc_id'         =>  env('MEXSMS_LEANCLOUD_LC_ID', ''),
+        'sms_lc_key'        =>  env('MEXSMS_LEANCLOUD_LC_KEY', '')
     ],
 
     // 阿里云配置
     'aliyun'    =>  [
-        'sign_name'         =>  env('MEXSMS_ALIYUN_SIGN_NAME', '桜十玖'),
-        'access_key_id'     =>  env('MEXSMS_ALIYUN_ACCESS_KEY_ID', 'LTAIpp90ilA2elhz'),
-        'access_key_secret' =>  env('MEXSMS_ALIYUN_ACCESS_KEY_SECRET', 'wuFWdv7b63DN0DUqOAsoo9g59ni45G'),
-        'template_code'     =>  env('MEXSMS_ALIYUN_TEMPLATE_CODE', 'SMS_168586548'),
+        'sign_name'         =>  env('MEXSMS_ALIYUN_SIGN_NAME', ''),
+        'access_key_id'     =>  env('MEXSMS_ALIYUN_ACCESS_KEY_ID', ''),
+        'access_key_secret' =>  env('MEXSMS_ALIYUN_ACCESS_KEY_SECRET', ''),
+        'template_code'     =>  env('MEXSMS_ALIYUN_TEMPLATE_CODE', ''),
     ],
 
 
